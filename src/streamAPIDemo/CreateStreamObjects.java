4e5f77f91a0196d9de0bc50829b23aea72b31487
rubenpa1998 <rubenpa1998@gmail.com>
2023-08-03 13:28:20 +0530

    Update : Added diff sources

--- src/streamAPIDemo/CreateStreamObjects.java
+++ src/streamAPIDemo/CreateStreamObjects.java
@@ -1,5 +1,7 @@
 package streamAPIDemo;
 
+import java.lang.reflect.Array;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class CreateStreamObjects {
@@ -9,6 +11,19 @@ public class CreateStreamObjects {
         //Create a Stream
         Stream<String> stream = Stream.of("a","b","c");
         stream.forEach(System.out::println);
-        
+
+        //Create a stream from diff sources
+        Collection<String> collection = Arrays.asList("Java","Python","C#");
+        Stream<String> stream1 = collection.stream();
+        stream1.forEach(System.out::println);
+
+        List<String> list = Arrays.asList("Java","Python","C#");
+        Stream<String> stream2 = list.stream();
+        stream2.forEach(System.out::println);
+
+        Set<String> set = new HashSet<>(list);
+        Stream<String> stream3 = set.stream();
+        stream3.forEach(System.out::println);
+
     }
 }
