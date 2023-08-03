0a5b091e9b777625b6a7d64631c62707565c1807
rubenpa1998 <rubenpa1998@gmail.com>
2023-08-03 13:22:18 +0530

    Update: Create a Stream

--- src/streamAPIDemo/CreateStreamObjects.java
+++ src/streamAPIDemo/CreateStreamObjects.java
@@ -1,4 +1,14 @@
 package streamAPIDemo;
 
+import java.util.stream.Stream;
+
 public class CreateStreamObjects {
+
+    public static void main(String[] args) {
+
+        //Create a Stream
+        Stream<String> stream = Stream.of("a","b","c");
+        stream.forEach(System.out::println);
+        
+    }
 }
