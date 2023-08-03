35700046cb0c95feca7e78c61b0f73af2b46adc6
rubenpa1998 <rubenpa1998@gmail.com>
2023-08-03 14:30:49 +0530

    Update
    Added Getter and Setter
    Added Constructor
    Added toString()
    Added List<Product> with getProduct() method
    Added filter for the price > 25000 in the traditional ways

---
+++ src/streamAPIDemo/StreamFIlterDemo.java
@@ -0,0 +1,78 @@
+package streamAPIDemo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamFIlterDemo {
+    public static void main(String[] args) {
+        List<Product> list = new ArrayList<Product>();
+
+        for(Product product : getProducts()) {
+            if(product.getPrice() > 25000){
+                list.add(product);
+            }
+        }
+        for(Product product : list) {
+            System.out.println(product);
+        }
+
+    }
+
+    private static List<Product> getProducts(){
+
+        List<Product> productList = new ArrayList<Product>();
+        productList.add(new Product(1,"HP Laptop",25000));
+        productList.add(new Product(2,"Dell Laptop",30000));
+        productList.add(new Product(3,"Lenovo Laptop",28000));
+        productList.add(new Product(3,"Sony Laptop",28000));
+        productList.add(new Product(5,"Apple Laptop",90000));
+        return productList;
+
+    }
+
+}
+
+class Product {
+    private int id;
+    private String name;
+    private float price;
+
+    public Product(int id, String name, float price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
